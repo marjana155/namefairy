ca685931889ac762a1d79b275a0c6f62fd6bc6a7
marjana155 <marjanajuthi.155@gmail.com>
2021-07-13 01:09:50 +0600

    modified templates

--- names/views.py
+++ names/views.py
@@ -16,12 +16,20 @@ def home(request):
 def action(request):
     gender = request.GET["gender"]
     category = request.GET["category"]
+    letter = request.GET["letter"]
     cat = get_object_or_404(Category, title=category)
     gen = get_object_or_404(Gender, title=gender)
     names = Name.objects.filter(category=cat, gender=gen)
     namelist = []
     for name in names:
-        namelist.append(name.name)
-    name = random.choice(namelist)
+        if letter != '':
+            if name.name[0] == letter.upper():
+                namelist.append(name.name)
+        else:
+            namelist.append(name.name)
+    if namelist != []:
+        name = random.choice(namelist)
+    else:
+        name = "Sorry!! we don't have any name with your given criteria yet"
 
     return render(request, 'action.html', {'name': name})
